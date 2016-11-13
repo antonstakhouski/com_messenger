9acb8611483ac5c95d494fc904788c53e85691c8
Anton Stakhouski 450502 <anton.stakhouski@gmail.com>
2016-11-13 22:34:25 +0300

    csma/cd example implementation

--- src/main.py
+++ src/main.py
@@ -2,13 +2,17 @@
 
 from PyQt5.QtCore import Qt, QIODevice
 from PyQt5.QtWidgets import QGridLayout, QLabel, QRadioButton, QTextEdit, QWidget, QTextBrowser, QPushButton, \
-    QVBoxLayout, QLineEdit
+    QVBoxLayout
 from PyQt5.QtSerialPort import QSerialPort
+import time
+import random
+
+CODING = 'utf8'
 
 
-class COMportMessenger(QWidget):
+class ComPortMessenger(QWidget):
     def __init__(self, parent=None):
-        super(COMportMessenger, self).__init__(parent)
+        super(ComPortMessenger, self).__init__(parent)
 
         self.sender_name = "/dev/tnt1"
         self.receiver_name = "/dev/tnt0"
@@ -31,13 +35,13 @@ class COMportMessenger(QWidget):
         debug_label = QLabel("Debug info:")
         self.debug_text = QTextBrowser()
 
-        self.button = QRadioButton("On - sender mode " + self.sender_name
-                                   + ", off - receiver mode " + self.receiver_name)
-        self.button.toggled.connect(self.handle_radio_button)
+        self.radio_button = QRadioButton("On - sender mode " + self.sender_name
+                                         + ", off - receiver mode " + self.receiver_name)
+        self.radio_button.toggled.connect(self.handle_radio_button)
 
         # grid layout initialisation
         grid_layout = QGridLayout()
-        grid_layout.addWidget(self.button, 0, 0, Qt.AlignRight)
+        grid_layout.addWidget(self.radio_button, 0, 0, Qt.AlignRight)
         grid_layout.addWidget(self.init_button, 0, 1, Qt.AlignCenter)
         grid_layout.addWidget(input_label, 1, 0, Qt.AlignHCenter)
         grid_layout.addWidget(self.input_text, 2, 0)
@@ -58,8 +62,20 @@ class COMportMessenger(QWidget):
         self.setLayout(main_layout)
         self.setWindowTitle("Simple COM port messenger")
 
-        # serial port initialization
         self.com = QSerialPort()
+        self.serial_port_init()
+        # collision window in millis
+        self.collision_window = 70
+        self.jam_signal_time = 35
+        self.slot_time = int((self.collision_window + self.jam_signal_time) * 1.25)
+        self.max_tries = 10
+        self.jam_signal = bytes(str(chr(7)), CODING)
+        self.escape_signal = bytes(str(chr(6)), CODING)
+        self.previous_byte = int()
+        self.output_string = str()
+
+    def serial_port_init(self):
+        # serial port initialization
         self.com.setBaudRate(QSerialPort.Baud115200)
         self.com.setDataBits(QSerialPort.Data8)
         self.com.setStopBits(QSerialPort.OneStop)
@@ -67,7 +83,7 @@ class COMportMessenger(QWidget):
 
     # sender/receiver mode switch
     def handle_radio_button(self):
-        if self.button.isChecked():
+        if self.radio_button.isChecked():
             if self.com.isOpen():
                 self.com.close()
             self.com.setPortName(self.sender_name)
@@ -80,27 +96,77 @@ class COMportMessenger(QWidget):
 
     # port initialization and opening
     def handle_init_button(self):
-        if self.button.isChecked():
+        if self.radio_button.isChecked():
             self.com.setPortName(self.sender_name)
             self.com.open(QIODevice.WriteOnly)
         else:
             self.com.setPortName(self.receiver_name)
+            self.com.open(QIODevice.ReadOnly)
             self.com.readyRead.connect(self.handle_receive)
         self.debug_text.append("Port was initialized")
 
     # send message button handle
     def handle_send_button(self):
-        if self.button.isChecked():
+        if self.radio_button.isChecked():
             text = self.input_text.toPlainText()
-            self.com.writeData(bytes(text, 'utf8'))
             self.input_text.clear()
+            if len(text):
+                for symbol in text:
+                    try_counter = 0
+                    successful_transfer = False
+                    while not successful_transfer:
+                        seconds = self.get_current_second()
 
-    # receive message button handle
+                        while not (seconds % 2):
+                            time.sleep(1)
+                            seconds = self.get_current_second()
+
+                        self.com.writeData(bytes(symbol, CODING))
+                        time.sleep(self.collision_window / 1000.0)
+                        if not (self.get_current_second() % 2):
+                            self.debug_text.append("x")
+                            self.com.writeData(self.jam_signal)
+                            try_counter += 1
+                            # add slot time waiting
+                            if try_counter > 10:
+                                self.output_text.append("Error. Number of tries expired")
+                                return
+                            k = min(try_counter, self.max_tries)
+                            random.seed()
+                            slot_count = random.randint(0, 2 ** k)
+                            time.sleep(slot_count * self.slot_time / 1000)
+                        else:
+                            successful_transfer = True
+            self.com.writeData(self.escape_signal)
+
+    @staticmethod
+    def get_current_second():
+        time_tuple = time.localtime(time.time())
+        seconds = time_tuple[5]
+        return seconds
+
+    # receive message handle
     def handle_receive(self):
-        if not self.button.isChecked():
-            ba = self.com.readAll()
-            string = str(ba, 'utf-8')
-            self.output_text.append(string)
+        if not self.radio_button.isChecked():
+            byte_array = bytes(self.com.readAll())
+            no_previous_byte = True
+            self.output_string = ""
+
+            for new_byte in byte_array:
+                if new_byte == self.jam_signal[0]:
+                    no_previous_byte = True
+                    continue
+                if new_byte == self.escape_signal[0]:
+                    if not no_previous_byte:
+                        self.output_string += chr(self.previous_byte)
+                    no_previous_byte = True
+                else:
+                    if not no_previous_byte:
+                        self.output_string += chr(self.previous_byte)
+                    else:
+                        no_previous_byte = False
+                    self.previous_byte = new_byte
+            self.output_text.append(self.output_string)
 
 
 if __name__ == '__main__':
@@ -110,7 +176,7 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
 
-    messenger = COMportMessenger()
+    messenger = ComPortMessenger()
     messenger.show()
 
     sys.exit(app.exec_())
